ca849678fe311dd792bc63859caedfde0a5656d5
taslimmuhammed <taslimmuhammed67@gmail.com>
2023-06-08 06:56:54 +0530

    contracts

--- src/Contracts/NFTStake.sol
+++ src/Contracts/NFTStake.sol
@@ -1,48 +1,88 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+pragma solidity ^0.8.4;
+
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract StakingContract is Ownable {
-    IERC20 public stakingToken;
-    IERC1155 public nftContract;
-    uint256 public stakeThreshold;
-    uint256 public nextTokenId;
-    
-    mapping(address => uint256) public stakes;
-    
-    event Staked(address indexed user, uint256 amount);
-    event NFTClaimed(address indexed user, uint256 tokenId);
+// @title NFT Staking 
+/// @author Karan J Goraniya
+/// @notice You can use this contract for only the most basic simulation
+/// @dev All function calls are currently implemented without side effects
 
-    constructor(address _stakingToken, address _nftContract, uint256 _stakeThreshold) {
-        stakingToken = IERC20(_stakingToken);
-        nftContract = IERC1155(_nftContract);
-        stakeThreshold = _stakeThreshold;
-        nextTokenId = 1;
+contract Staking is ERC1155Holder {
+
+    IERC1155 public NFTItem;
+    using SafeERC20 for IERC20;
+    IERC20 private token;
+    uint256 decimals = 10**18;    
+    struct StakeStruct{
+        uint256 amount;
+        uint256 timestamp;
+    }
+    struct User{
+        StakeStruct[] stakes;
+        uint256 id;
+    }
+    mapping(address => User) public users;
+
+    constructor(address _token,address _NFTItem) {
+        token = IERC20(_token);
+        NFTItem = IERC1155(_NFTItem);
     }
 
-    function stake(uint256 _amount) external {
-        require(_amount > 0, "Amount must be greater than zero");
-        require(stakingToken.balanceOf(msg.sender) >= _amount, "Insufficient staking token balance");
-        
-        stakingToken.transferFrom(msg.sender, address(this), _amount);
-        stakes[msg.sender] += _amount;
-        
-        emit Staked(msg.sender, _amount);
-        
-        if (stakes[msg.sender] >= stakeThreshold) {
-            uint256 numNFTsToClaim = stakes[msg.sender] / stakeThreshold;
-            
-            for (uint256 i = 0; i < numNFTsToClaim; i++) {
-                nftContract.mint(msg.sender, nextTokenId, 1, "");
-                emit NFTClaimed(msg.sender, nextTokenId);
-                nextTokenId++;
-            }
-            
-            stakes[msg.sender] %= stakeThreshold;
+    event Stake(address indexed owner, uint256 id, uint256 amount, uint256 time);
+
+
+    // @notice It will calculate the rate of the token reward
+    // @dev It will block.timestamp to track the time.
+    // @return Return the reward rate %
+
+
+
+
+    // @notice It will give user to stake the NFT.
+    // @dev It will confirm the you have enough NFT to stake.
+    // @param It will take Token Id of NFT & Amount.
+
+    function stakeNFT( uint256 _amount) public {
+        require(NFTItem.balanceOf(msg.sender, 0) >= _amount,"you dont have enough balance");
+        NFTItem.safeTransferFrom(msg.sender, address(this), 0, _amount, "0x00");
+        users[msg.sender].stakes.push(StakeStruct( _amount, block.timestamp));
+        emit Stake (msg.sender, 0, _amount, block.timestamp);
+    }
+ 
+    function getDCToken() public {
+        uint256 reward = calculateReward(msg.sender);
+        handleTimestamps();
+        token.safeTransfer( msg.sender, reward);
+    }
+    function calculateReward(address _user) public  view returns(uint256) {
+        uint256 reward;
+        StakeStruct[] memory array = users[_user].stakes;
+        if(array.length == 0) return 0;
+        for (uint i = 0; i < array.length; i++) {
+            uint256 timeDiff = block.timestamp - array[i].timestamp;
+            timeDiff = timeDiff / 2 days;
+            reward +=array[i].amount*timeDiff;
+        }
+        return reward;
+    }
+    function handleTimestamps() internal {
+        if(users[msg.sender].stakes.length == 0) return;
+        for (uint i = 0; i < users[msg.sender].stakes.length; i++) {
+            users[msg.sender].stakes[i].timestamp = block.timestamp;
         }
     }
+    function unStakeNFT() public {
+        StakeStruct[] memory array = users[msg.sender].stakes;
+        if(users[msg.sender].stakes.length == 0) return;
+        for (uint i = 0; i < array.length; i++) {
+                NFTItem.safeTransferFrom(address(this), msg.sender, 0, array[i].amount, "0x00");
+        }
+        delete users[msg.sender].stakes;
+    }
+
 }
