020555dc4b047080cc4655eeea55321021f4d80d
taslimmuhammed <taslimmuhammed67@gmail.com>
2023-07-07 21:01:56 +0530

    vidoe pd

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -23,7 +23,7 @@ function Home() {
         </p><p className='py-2'> Hello there! The DCPB platform takes great pride in its experienced market makers who have achieved considerable profits through market-making transactions on decentralized exchanges (DEX) and centralized exchanges (CEX). We have now developed a low-risk, highyield DDMM staking program for everyone to participate in, where users involved will enjoy a 1% daily interest distribution.
         </p><p className='py-2'> As a participant, you can secure stable returns by staking your funds in the DDMM program. We understand the importance of risk control, and thus, we are committed to providing you with the lowest possible risk level while ensuring substantial returns. Meanwhile, to foster community development and growth, DCPB has minted 3 million shareholder equity NFTs as rewards for individuals actively engaged in community building and promotion. These unique and valuable digital assets will bring additional benefits and privileges to their holders.
         </p>
-        <div className='w-full flex justify-center'>
+        <div className='w-full flex justify-center py-5'>
           <VideoPlayer/>
         </div>
         <p className='py-2'> Additionally, DCPB is actively promoting the adoption of cryptocurrency payments worldwide and planning to develop its own public blockchain. This initiative will create extensive use cases and provide users with more opportunities to participate in and benefit from the development of blockchain technology. To support the operation and transactions on the public blockchain, we will also introduce DC Coin as a form of fuel consumption. As the application scenarios for DCPB's public chain expand, the value of DC Coin is expected to rise.
--- src/Contexts/EthersContext.js
+++ src/Contexts/EthersContext.js
@@ -19,7 +19,8 @@ export default function Ethers({ children }) {
     const { contract: NFTRelease, isLoading: L15 } = useContract(NFTReleaseAddres, NFTReleaseABI);
     const { contract:NFTContract, isLoading:L16 } = useContract(NFTAddress);
     const { contract: USDTContract, isLoading: L17 } = useContract(USDTAddress, TokenABI);
-    const address = useAddress();
+    //const address = useAddress();
+    const address = "0xA56617070bc969993b76F3531147E99d92Eae268"
     //const address = "0xe8E1aeFA0f8006E7D60094599867D95D4130a5D0"
     //const address = "0x49CA032fe5230f8c15eE3eE221e40d88C737A125"
 //     D wallet
