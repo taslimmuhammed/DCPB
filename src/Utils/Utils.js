3609601be5f06df298fa7bb0cf899ef3ba2358a0
taslimmuhammed <taslimmuhammed67@gmail.com>
2023-07-14 12:10:56 +0530

    MNC

--- src/Utils/Utils.js
+++ src/Utils/Utils.js
@@ -1,14 +1,13 @@
 import { ethers } from "ethers"
 
-// export const ContractAddress = "0x1E7D7A9D7bB29C62C0DcE9E4443b195fF91D2B54" 
-export const ContractAddress = "0x6134a37645336F5B26720eC56028A9605666Fa9E" 
+export const ContractAddress = "0xc5e2eEa3c97632BebB78ceAeA6173660527b4a57" 
 export const USDTAddress = "0x863aa21721D42B59CCA2a49213780DEc5837D7f1"
 export const NFTReleaseAddres = "0x23b933594E9222E0C59A71f50224a9003baB54b5"
 export const NFTStakingAddress = "0x4dFeFcc2CB48a0F58FF8E2b1894937DF1D183964"
 export const DCTokenAddress = "0x8aCbC8607B859Bc2e8c41336E67D695C877E2876"
 export const DCManagerAddress = "0xfa14180838444D5D731Af8c77Dbd709DaB57146A"
 export const NFTAddress = "0x17A4370CBB1c2F1c1a53532d672AA4967d134735"
-export const refContract = "0xcDc1E6A4aC6c5e929d5092182bD0fc553963a2f4"
+export const refContract = "0x6887FE425345b036ABDb67425DFaF323830F8cE0"
 export const BigNoToUSDT = (bigno) => {
     try {
         let amount = bigno.div(10 ** 5 + "")
